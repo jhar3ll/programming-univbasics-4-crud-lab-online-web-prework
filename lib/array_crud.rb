b8ce32617ae9c9271929457be15a7985ca8b352d
jharell chambers <jharell.chambers@selu.edu>
2019-07-27 00:39:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,8 +18,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  our_animals = ["simon", "luna", "charlie", "lila"]
-  our_animals.pop( "eli")
+ array = ["wow", "I", "am", "really", "learning"]
+ array.pop
 end
 
 def remove_element_from_start_of_array(array)
