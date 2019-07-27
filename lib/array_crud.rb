68a6cdc563e61e4454e7e92044814358284a7b1e
jharell chambers <jharell.chambers@selu.edu>
2019-07-27 00:03:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -4,62 +4,50 @@ end
 
 def create_an_array
   our_animals = ["simon", "luna", "charlie", "lila"]
+  p our_animals
 end
-
-p our_animals
 
 def add_element_to_end_of_array(array, element)
   our_animals = ["simon", "luna", "charlie", "lila"]
   our_animals << "eli"
+  p our_animals
 end
-
-p our_animals
 
 def add_element_to_start_of_array(array, element)
   our_animals = ["simon", "luna", "charlie", "lila"]
   our_animals.unshift( "eli")
+  p our_animals
 end
-
-p our_animals
 
 def remove_element_from_end_of_array(array)
   our_animals = ["simon", "luna", "charlie", "lila"]
   our_animals.pop( "eli")
+  p our_animals
 end
-
-p our_animals
 
 def remove_element_from_start_of_array(array)
   our_animals = ["simon", "luna", "charlie", "lila"]
   our_animals.shift( "eli")
+  p our_animals
 end
-
-p our_animals
 
 def retrieve_element_from_index(array, index_number)
   our_animals = ["simon", "luna", "charlie", "lila"]
-  our_animals[2]
+  p our_animals[2]
 end
-
-p our_animals
 
 def retrieve_first_element_from_array(array)
   our_animals = ["simon", "luna", "charlie", "lila"]
-  our_animals[0]
+  p our_animals[0]
 end
-
-p our_animals
 
 def retrieve_last_element_from_array(array)
   our_animals = ["simon", "luna", "charlie", "lila"]
-  our_animals[-1]
+ p  our_animals[-1]
 end
-
-p our_animals
 
 def update_element_from_index(array, index_number, element)
   our_animals = ["simon", "luna", "charlie", "lila"]
   our_animals[-1] = "gene"
+  p our_animals
 end
-
-p our_animals
