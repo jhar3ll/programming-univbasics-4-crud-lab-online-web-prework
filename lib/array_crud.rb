8bb079fc27deec927488534db4b67ef8c9549d03
jharell chambers <jharell.chambers@selu.edu>
2019-07-27 00:45:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -38,8 +38,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  our_animals = ["simon", "luna", "charlie", "lila"]
-  our_animals[-1]
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
