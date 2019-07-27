91f4241db311bc3db7d275aab4cfc79f00613a67
jharell chambers <jharell.chambers@selu.edu>
2019-07-27 00:43:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  our_animals = ["simon", "luna", "charlie", "lila"]
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
   our_animals[2]
 end
 
