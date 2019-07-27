b76a0862cc2d2c37334de54978b77cad8418288f
jharell chambers <jharell.chambers@selu.edu>
2019-07-27 00:33:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  our_animals = ["simon", "luna", "charlie", "lila"]
+  our_animals2 = ["simon", "luna", "charlie", "lila"]
+  
 end
 
 def add_element_to_start_of_array(array, element)
