eff023952ebc08127fef0815a1b3ed1180cb5bfd
jharell chambers <jharell.chambers@selu.edu>
2019-07-27 00:43:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -24,7 +24,7 @@ end
 
 def remove_element_from_start_of_array(array)
  array = ["wow", "I", "am", "really", "learning", "arrays!"]
- 
+ new_array = array.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
